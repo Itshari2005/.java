1f4462850781e97cf2d3fea32ab5b9ad77dd2e62
Itshari2005 <117710059+Itshari2005@users.noreply.github.com>
2024-10-17 10:10:29 +0530

    Update PosOrNegExample1.java
    Positive or Negative

--- PosOrNegExample1.java
+++ PosOrNegExample1.java
@@ -3,18 +3,19 @@ import java.util.Scanner;
 class PosOrNegExample1{
     int a;
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         System.out.println("Finding Positive or Negative number..!!");
+        System.out.print("Enter a number : ");
+        Scanner sc = new Scanner(System.in);
         PosOrNegExample1 obj = new PosOrNegExample1();
         obj.a = sc.nextInt();
         if(obj.a==0){
             System.out.println("The number is Zero i.e either positive nor negative");
         }
         else if(obj.a>0){
-            System.out.println("The number is Positive");
+            System.out.println("The number " + obj.a + " is Positive");
         }
         else{
-            System.out.println("The number is Negative");
+            System.out.println("The number" + obj.a + " is Negative");
         }
     }
 }
